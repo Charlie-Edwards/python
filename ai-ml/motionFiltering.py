bf51be917115942b2190de167f474a38b47f3e91
Charlie Edwards <285464439+Charlie-Edwards@users.noreply.github.com>
2026-07-12 16:59:04 +0200

    Create motionFiltering.py

---
+++ ai-ml/motionFiltering.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+video = cv.VideoCapture('motion.mp4')
+subtractor = cv.createBackgroundSubtractorMOG2(200, 50)
+
+while True:
+
+    ret, frame = video.read()
+
+    if ret:
+        mask = subtractor.apply(frame)
+        cv.imshow('Mask', mask)
+
+        if cv.waitKey(5) == ord('x'):
+            break
+
+    else:
+        video = cv.VideoCapture('motion.mp4')
+
+cv.destroyAllWindows()
+video.release()
